bccb4d9f19ba5ab1ad269ec7c06dd44bea438a65
SowingHustle <hazewry00@gmail.com>
2024-03-20 17:34:06 -0300

    style(error): Added some styling to the 404 page

--- views/auth/login.php
+++ views/auth/login.php
@@ -9,11 +9,12 @@
 </head>
 
 <body>
-
-
     <h1>
         <?php echo $this->view->title ?>
     </h1>
+    <div class="container">
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
