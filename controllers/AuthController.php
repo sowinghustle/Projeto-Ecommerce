14181200ee2b078b3be56f5af3242648447676ab
Leandro Cruz <LeandroCruz717@outlook.com>
2024-03-20 17:12:13 -0300

    style: alterations and fixes about register and signin names and folders

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,20 +4,15 @@ require_once "BaseController.php";
 
 class AuthController extends BaseController
 {
-    public function index()
-    {
-        $this->view->title = "Home";
-        include "views/home/index.php";
-    }
     public function login()
     {
         $this->view->title = "Login";
-        include "views/login/login.php";
+        include "views/auth/login.php";
     }
     public function register()
     {
         $this->view->title = "Signin";
-        include "views/login/signin.php";
+        include "views/auth/signin.php";
     }
     public function logout()
     {
