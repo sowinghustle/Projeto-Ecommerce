f80262782ea8930c80f3a8eb481752570d52704b
SowingHustle <hazewry00@gmail.com>
2024-03-20 17:00:10 -0300

    feat(routes): Added the remaining routes for the log, sign a logout pages, as well as their base content

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,9 +4,24 @@ require_once "BaseController.php";
 
 class AuthController extends BaseController
 {
-    public function index() 
+    public function index()
     {
         $this->view->title = "Home";
         include "views/home/index.php";
+    }
+    public function login()
+    {
+        $this->view->title = "Login";
+        include "views/login/login.php";
+    }
+    public function register()
+    {
+        $this->view->title = "Signin";
+        include "views/login/signin.php";
+    }
+    public function logout()
+    {
+        $this->view->title = "Logout";
+        header("location:login");
     }
 }
--- views/login/signin.php
+++ views/login/signin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <title>Login</title>
+    <title>Sign-in</title>
 </head>
 
 <body>
