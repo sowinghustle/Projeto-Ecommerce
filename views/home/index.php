d2b05e0784f0522035e6e4cdd64b44f2706038c8
SowingHustle <hazewry00@gmail.com>
2024-03-20 15:51:31 -0300

    build(index): added the bootstrap cdn to the project

--- views/home/index.php
+++ views/home/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <title>Home</title>
 </head>
 
@@ -12,6 +13,7 @@
     <h1>
         <?php echo $this->view->title ?>
     </h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
