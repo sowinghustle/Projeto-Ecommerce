b2e44aeb187556a4fccd1050cd8510c48da54102
SowingHustle <hazewry00@gmail.com>
2024-03-20 17:20:27 -0300

    style(css): added the css reset and css index files and their references in the views pages.

--- views/home/index.php
+++ views/home/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/index.css" />
     <title>Home</title>
 </head>
 
