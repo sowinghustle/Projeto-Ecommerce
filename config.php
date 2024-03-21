ffe372e26a610e43238c9bdea6b9e1d688c51915
Leandro Cruz <LeandroCruz717@outlook.com>
2024-03-21 14:03:42 -0300

    refactor: create config.php to declare constants (like database connection config), and update database to return false instead of using echo or throwing errors in failed operations and add hasError and getError methods to Database model

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_HOST", "db");
+define("DB_NAME", "db");
+define("DB_USER", "user");
+define("DB_PASSWORD", "you_dont_deserve_to_lives");
--- models/Database.php
+++ models/Database.php
@@ -2,10 +2,11 @@
 
 class Database
 {
-    private $host = "db";
-    private $db = "db";
-    private $login = "user";
-    private $pass = "you_dont_deserve_to_live";
+    private $host = DB_HOST;
+    private $db = DB_NAME;
+    private $login = DB_USER;
+    private $pass = DB_PASSWORD;
+    private $error = null;
     private $conn;
 
     public function __construct()
@@ -14,24 +15,44 @@ class Database
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->login, $this->pass);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+            $this->error = $e;
         }
+    }
+
+    public function hasError()
+    {
+        return $this->error != null;
+    }
+
+    public function getError()
+    {
+        return $this->error;
     }
 
     public function query($query)
     {
+        if ($this->conn == null)
+            return false;
+
+        $this->error = null;
+
         try {
             $stmt = $this->conn->query($query);
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             return $data;
         } catch (PDOException $e) {
-            echo "Query failed: " . $e->getMessage();
+            $this->error = $e;
             return false;
         }
     }
 
     public function exec($query)
     {
+        if ($this->conn == null)
+            return false;
+
+        $this->error = null;
+
         try {
             return $this->conn->exec($query);
         } catch (PDOException $e) {
