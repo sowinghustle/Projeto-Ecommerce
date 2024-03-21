a68c3474de2012b5a0d3369f138fdbce43603e1b
Leandro Cruz <LeandroCruz717@outlook.com>
2024-03-21 14:11:08 -0300

    chore(db.sql): add a pre-defined user to database on first creation

--- db.sql
+++ db.sql
@@ -5,3 +5,13 @@ CREATE TABLE client (
     password text not null,
     PRIMARY KEY (id)
 );
+
+INSERT INTO client (
+    username, 
+    email,
+    password
+) VALUES (
+    "user",
+    "user@admin.com",
+    "12345678"
+);
