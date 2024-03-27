ed4626b1f4203154a842c57a629203be60c121aa
Leandro Cruz <LeandroCruz717@outlook.com>
2024-03-27 17:37:57 -0300

    feat(client): implement findByUsernameAndPassword in model

--- models/Client.php
+++ models/Client.php
@@ -14,14 +14,55 @@ class Client
         $this->id = $id;
         $this->username = $username;
         $this->email = $email;
-        $this->changePassword($password);
+
+        if ($password)
+            $this->changePassword($password);
+    }
+
+    public function findByUsernameAndPassword($isPasswordOptional = false)
+    {
+        $username = $this->username;
+        $password = $this->password;
+
+        if (!$username) {
+            return [false, "you should provide valid username"];
+        }
+
+        if (!$isPasswordOptional && !$password) {
+            return [false, "you should provide valid password"];
+        }
+
+        try {
+            $conn = new Database();
+            $stmt = $conn->pdo->prepare("SELECT c.id, c.username, c.password, c.email FROM client c WHERE c.username=:username AND c.password=IF(:is_password_optional, c.password, :password)");
+            $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+            $stmt->bindValue(":is_password_optional", $isPasswordOptional, PDO::PARAM_BOOL);
+            $stmt->bindValue(":password", $password, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($result) {
+                $this->id = $result["id"];
+                $this->username = $result["username"];
+                $this->email = $result["email"];
+                $this->password = $result["password"];
+
+                return [true, true];
+            }
+
+            return [true, false];
+        } catch (PDOException $e) {
+            return array(false, "Sorry, we couldn't connect to the database. Please, try later...");
+        } catch (Exception $e) {
+            return array(false, "Sorry, something went wrong, and was not possible to proccess your request!");
+        }
     }
 
     public function save()
     {
         try {
             $conn = new Database();
-
             $pdo = null;
 
             if ($this->id == 0) {
@@ -39,15 +80,15 @@ class Client
             if ($this->id == 0) {
                 $stmt = $conn->pdo->prepare("SELECT @id AS id");
                 $stmt->execute();
-
                 $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
                 $this->id = $result["id"];
             }
 
             return array(true, null);
         } catch (PDOException $e) {
-            return array(false, "unexpected exception " . $e->getMessage());
+            return array(false, "Sorry, we couldn't connect to the database to save the data. Please, try again later...");
+        } catch (Exception $e) {
+            return array(false, "Sorry, something went wrong, and was not possible to proccess your request!");
         }
     }
 
@@ -78,6 +119,8 @@ class Client
 
     public function changePassword($newPassword)
     {
-        $this->password = Hash::make($newPassword);
+        // $this->password = Hash::make($newPassword);
+        // TODO: add again later, but checking the hashing results and comparison
+        $this->password = $newPassword;
     }
 }
