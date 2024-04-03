57e95201b8d17562711797966711b6a065ba589e
SowingHustle <hazewry00@gmail.com>
2024-04-03 17:43:17 -0300

    feat(homeController): added the session data for the userId on the home page

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,11 +3,14 @@
 require_once "BaseController.php";
 require_once "models/Database.php";
 require_once "models/Client.php";
+require_once "models/Session.php";
 
 class HomeController extends BaseController
 {
     public function index()
     {
+        $session = new Session();
+        $userId = $session->get('usuario-logado');
         $this->view->title = "Home";
         include "views/home/index.php";
     }
