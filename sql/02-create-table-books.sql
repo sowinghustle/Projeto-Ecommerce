ed172aec8d347e8a5f6ea52a789b8204002f1f69
Leandro Cruz <LeandroCruz717@outlook.com>
2024-04-10 14:16:53 -0300

    chore(sql): add user id in books table

--- sql/02-create-table-books.sql
+++ sql/02-create-table-books.sql
@@ -3,8 +3,9 @@ CREATE TABLE books (
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255),
     description TEXT,
-    isbn VARCHAR(255) UNIQUE NOT NULL,
     categories VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (id)
+    user INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(user) REFERENCES users(id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
