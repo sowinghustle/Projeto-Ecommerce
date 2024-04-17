1457c50285459e23fb1758df838fa32cd1736986
Leandro Cruz <LeandroCruz717@outlook.com>
2024-04-17 01:26:47 -0300

    chore(database): change password heathers musical reference

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 define("DB_HOST", "ecommerce-db");
 define("DB_NAME", "ecommerce-db");
 define("DB_USER", "user");
-define("DB_PASSWORD", "you_dont_deserve_to_live");
+define("DB_PASSWORD", "unsafe_password");
 define("PRODUCTION", false);
