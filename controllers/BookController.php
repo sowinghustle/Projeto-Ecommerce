6cc990e4d788479dc1cacaf18eab6586239f6b6f
SowingHustle <hazewry00@gmail.com>
2024-05-29 17:34:06 -0300

    feat(BookBuilder): implemented the factory method and the builder method to the book class.

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -4,6 +4,7 @@ require_once "BaseController.php";
 require_once "models/Database.php";
 require_once "models/Book.php";
 require_once "models/BookList.php";
+require_once "models/BookCardRenderer.php";
 
 class BookController extends BaseController
 {
