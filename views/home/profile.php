e176ca7d7f147920ec7b94d40a87c39ba374239b
SowingHustle <hazewry00@gmail.com>
2024-06-05 17:30:18 -0300

    style: created single css files for each one of the views, and added the href to each one of those in the respective views pages.

--- views/home/profile.php
+++ views/home/profile.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/index.css" />
+    <link rel="stylesheet" href="../../assets/css/home.css" />
     <title>
         <?php echo $this->view->title ?>
     </title>
